c56fd50ccf5ef1f5d950c2a7c1491ee4f3a10d7c
CodeKei02 <keicode.dev02@gmail.com>
2025-11-25 14:20:44 -0400

    feat(style): implements style prop

--- src/components/react/Button.tsx
+++ src/components/react/Button.tsx
@@ -5,11 +5,13 @@ export const Button = ({
   href,
   children,
   external,
+  style,
 }: {
   text: string;
   href: string;
   children?: React.ReactNode;
   external?: boolean;
+  style?: React.CSSProperties;
 }) => {
   const isExternal =
     typeof external === "boolean" ? external : /^(https?:)?\/\//.test(href);
@@ -20,6 +22,7 @@ export const Button = ({
       target={isExternal ? "_blank" : undefined}
       rel={isExternal ? "noopener noreferrer" : undefined}
       className="w-40 h-12 flex justify-center items-center bg-white cursor-pointer rounded-3xl border-2 border-[#000] shadow-[inset_0px_-2px_0px_1px_#000] group hover:bg-[#000] transition duration-300 ease-in-out"
+      style={style}
     >
       <span className="font-medium text-[#333] group-hover:text-white flex items-center gap-2">
         {children}
