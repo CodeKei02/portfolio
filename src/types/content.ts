ad64a095071d601cd3bc73c959f9731ad9e942e3
CodeKei02 <keicode.dev02@gmail.com>
2025-11-25 14:12:04 -0400

    feat(types): create types for content in JSON

---
+++ src/types/content.ts
@@ -0,0 +1,46 @@
+export interface PageMeta {
+  title: string;
+  description: string;
+}
+
+export interface HeroSubtitle {
+  experience: string;
+  description: string;
+}
+
+export interface Hero {
+  title: string;
+  subtitle: HeroSubtitle;
+}
+
+export interface Titles {
+  experience: string;
+  projects: string;
+  about: string;
+}
+
+export interface ExperienceItem {
+  date: string;
+  title: string;
+  company: string;
+  description: string;
+}
+
+export interface ProjectItem {
+  id: number;
+  name: string;
+  description: string;
+  tecnologies: string[];
+  url: string;
+  repository?: string;
+}
+
+export interface Content {
+  page: PageMeta;
+  hero: Hero;
+  titles: Titles;
+  experiences: ExperienceItem[];
+  projects: ProjectItem[];
+}
+
+export default Content;
