9c66270ef472c0cd3f2c6c6140858be3586a4b01
CodeKei02 <keicode.dev02@gmail.com>
2025-11-27 18:18:39 -0400

    chore: descomment elements and remove innecessary code

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,12 +2,29 @@ import React, { useEffect, useState } from "react";
 import { Entry } from "../../components/react/Entry";
 import { useShowEntry } from "@/store/useShowEntry";
 import Header from "../../components/react/Header";
+import { Experience } from "../../components/layouts/Experience";
+import useLanguageStore from "@/store/useLanguageStore";
+import { DEFAULT_LANGUAGE, type Language } from "@/support-languages";
+import en from "../../content/en/en.json";
+import es from "../../content/es/es.json";
+import { Projects } from "@/components/layouts/Projects";
+import { AboutMe } from "@/components/layouts/AboutMe";
+import { Footer } from "@/components/layouts/Footer";
+import navigation from "@/content/navigation.json";
+
+const CONTENT: Record<Language, any> = {
+  en,
+  es,
+};
 
 export default function HomePage({ duration = 1000 }: { duration?: number }) {
   const showEntry = useShowEntry((state) => state.showEntry);
   const setShowEntry = useShowEntry((state) => state.setShowEntry);
-
   const [isExiting, setIsExiting] = useState(false);
+  const lang = useLanguageStore((s) => s.lang);
+  const data = CONTENT[lang] ?? en;
+  const currentLang = lang || DEFAULT_LANGUAGE;
+  const navigationLinks = navigation.links[currentLang];
 
   useEffect(() => {
     const slideDuration = 1000;
@@ -42,7 +59,16 @@ export default function HomePage({ duration = 1000 }: { duration?: number }) {
           <Entry />
         </div>
       ) : (
-        <Header />
+        <>
+          <Header data={data} />
+          <div className="bg-black/90 h-auto">
+            <Experience data={data} title={data.titles.experience} />
+            <Projects data={data} title={data.titles.projects} />
+            <AboutMe title={data.titles.about} language={lang} />
+            <hr className="w-[90%] mt-6 mx-auto opacity-20 text-white" />
+            <Footer links={navigationLinks} />
+          </div>
+        </>
       )}
     </>
   );
